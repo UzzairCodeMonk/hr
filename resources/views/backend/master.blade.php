f5338cd61fee4af63138b9fffd96589a903f2cf3
Uzzair Baharudin <uzzaircode@gmail.com>
2018-12-07 11:28:30 +0800

    m

---
+++ resources/views/backend/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css">
+    <link rel="stylesheet" href="{{asset('css/core.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+
+<body class="min-h-fullscreen bg-img center-vh p-20" style="background-image: url(https://source.unsplash.com/OrwkD-iWgqg/1600x900)">
+    @yield('content')
+    <script type="text/javascript" src="{{asset('js/core.min.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/app.js')}}"></script>
+    <script type="text/javascript" src="{{asset('js/script.js')}}"></script>
+</body>
+
+</html>
