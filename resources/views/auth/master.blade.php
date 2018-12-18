416916a11bff5727075cd5a1bd98baa2bf579227
Uzzair Baharudin <uzzaircode@gmail.com>
2018-12-18 18:02:06 +0800

    add auth page title

--- resources/views/auth/master.blade.php
+++ resources/views/auth/master.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Datakraf HRMS</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css">
     <link rel="stylesheet" href="{{asset('css/core.min.css')}}">
