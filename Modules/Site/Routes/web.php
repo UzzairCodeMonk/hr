9583070b326f33fc81c8ca99ad374b9040743557
Uzzair Baharudin <uzzaircode@gmail.com>
2019-01-01 15:37:45 +0800

    add

--- Modules/Site/Routes/web.php
+++ Modules/Site/Routes/web.php
@@ -3,7 +3,7 @@
 
 Route::group(['prefix' => 'site', 'middleware' => 'auth'], function () {
 
-    Route::group(['prefix' => 'administrator'], function () {
+    Route::group(['prefix' => config('app.administration_prefix')], function () {
         Route::get('site-config', ['uses' => 'SitesController@index','as'=>'siteconfig.index']);
         Route::post('site-config/store', ['uses' => 'SitesController@store','as'=>'siteconfig.store']);
     });
