82261b71c8869628f41a0026eade036708a062de
Uzzair Baharudin <uzzaircode@gmail.com>
2019-01-14 15:45:31 +0800

    add siprates table

---
+++ Modules/Wage/Database/Migrations/2019_01_14_153301_create_siprates_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSipratesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('siprates', function (Blueprint $table) {
+            $table->increments('id');
+            $table->decimal('min_salary',13,2)->nullable();
+            $table->decimal('max_salary',13,2)->nullable();
+            $table->decimal('sip_employer_contribution',13,2)->nullable();
+            $table->decimal('sip_employee_contribution',13,2)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('siprates');
+    }
+}
