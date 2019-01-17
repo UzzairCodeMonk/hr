cf9f005e459c3c63f5bf9248947a5e022887fb62
Uzzair Baharudin <uzzaircode@gmail.com>
2019-01-17 17:09:37 +0800

    modify personal leave index view

--- Modules/Leave/Resources/views/leave/my-leave.blade.php
+++ Modules/Leave/Resources/views/leave/my-leave.blade.php
@@ -8,51 +8,55 @@ My Leave Applications
         <h3>My Leave Applications</h3>
     </div>
     <div class="card-body">
-        <table class="table table-bordered datatable">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Type</th>
-                    <th>Start Date</th>
-                    <th>End Date</th>
-                    <th class="text-center">Total Days</th>
-                    <th class="text-center">Status</th>
-                    <th class="text-center">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                @if(isset($results) && $results->count() > 0)
-                @foreach($results as $key=>$result)
-                <tr>
-                    <td>{{++$key}}</td>
-                    <td>{{$result->type->name ?? 'N/A'}}</td>
-                    <td>{{$result->start_date ?? 'N/A'}}</td>
-                    <td>{{$result->end_date ?? 'N/A'}}</td>
-                    <td class="text-center">{{$result->days_taken ?? 'N/A'}}</td>
-                    <td class="text-center"><span class="badge {{statusColor($result->status) ?? ''}}">{!! $result->status()->reason ?? 'N/A' !!}</span></td>
-                    <td class="text-center">
-                        <a href="{{URL::signedRoute('my-leave.show',['id'=>$result->id])}}" class="btn btn-sm" id="">
-                            View
-                        </a>
-                    </td>
-                </tr>
-                @endforeach
-                @else
-                <tr>
-                    <td colspan="4" class="text-center">No records found</td>
-                </tr>
-                @endif
-            </tbody>
-        </table>
+        <div class="table-responsive">
+            <table class="table table-bordered datatable">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Type</th>
+                        <th>Start Date</th>
+                        <th>End Date</th>
+                        <th class="text-center">Total Days</th>
+                        <th class="text-center">Status</th>
+                        <th class="text-center">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @if(isset($results) && $results->count() > 0)
+                    @foreach($results as $key=>$result)
+                    <tr>
+                        <td>{{++$key}}</td>
+                        <td>{{$result->type->name ?? 'N/A'}}</td>
+                        <td>{{$result->start_date ?? 'N/A'}}</td>
+                        <td>{{$result->end_date ?? 'N/A'}}</td>
+                        <td class="text-center">{{$result->days_taken ?? 'N/A'}}</td>
+                        <td class="text-center"><span class="badge badge-md {{statusColor($result->status) ?? ''}}">
+                                {{ ucwords($result->status() ?? 'Missing Status') }}</span></td>
+                        <td class="text-center">
+                            <a href="{{URL::signedRoute('my-leave.show',['id'=>$result->id])}}" class="btn btn-sm" id="">
+                                View
+                            </a>
+                        </td>
+                    </tr>
+                    @endforeach
+                    @else
+                    <tr>
+                        <td colspan="4" class="text-center">No records found</td>
+                    </tr>
+                    @endif
+                </tbody>
+            </table>
+        </div>
     </div>
 </div>
 @endsection
 
 @section('page-js')
-    @include('asset-partials.datatable')
-    <script type="text/javascript">
-        $(document).ready(function(){
-            $('.datatable').DataTable();
-        });
-    </script>
+@include('asset-partials.datatable')
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('.datatable').DataTable();
+    });
+
+</script>
 @endsection
