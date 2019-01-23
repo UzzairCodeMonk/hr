34f564543cf4a257d7fb9bfad83775bcb843fc80
Uzzair Baharudin <uzzaircode@gmail.com>
2019-01-23 10:15:19 +0800

    update user leave records

--- Modules/Leave/Resources/views/leave/admin/user-leaves.blade.php
+++ Modules/Leave/Resources/views/leave/admin/user-leaves.blade.php
@@ -1,11 +1,11 @@
 @extends('backend.master')
 @section('page-title')
-{{$leaves->first()->user->personalDetail->name ?? ''}}'s Leave Records
+{{$user->personalDetail->name ?? ''}}'s Leave Records
 @endsection
 @section('content')
 <div class="card">
     <div class="card-header">
-        <h3 class="cad-title">{{$leaves->first()->user->personalDetail->name ?? ''}}'s Leave Records</h3>
+        <h3 class="cad-title">{{$user->personalDetail->name ?? ''}}'s Leave Records</h3>
     </div>
     <div class="card-body">
         <table class="table table-striped table-bordered datatable">
