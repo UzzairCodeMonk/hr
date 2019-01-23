d40be978ae53f01e98a256279cf7a573f9412370
Uzzair Baharudin <uzzaircode@gmail.com>
2019-01-23 10:43:55 +0800

    update family details

--- Modules/Profile/Resources/views/partials/family/dynamic-form.blade.php
+++ Modules/Profile/Resources/views/partials/family/dynamic-form.blade.php
@@ -1,11 +1,10 @@
 @if(isset($family))
 <form action="{{route('family.update',['id'=>$family->id])}}" method="POST" enctype="multipart/form-data">
     @else
-    <form action="{{route('family.store')}}" method="POST" enctype="multipart/form-data"></form>
+    <form action="{{route('family.store')}}" method="POST" enctype="multipart/form-data">
     @endif
     @csrf
     <div class="dynamic-list w-20">
         @include('profile::partials.family.input')
     </div>
-
 </form>
