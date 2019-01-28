f4d7d2137b6293eb2af5a1a3d70318d7ebf5e478
Uzzair Baharudin <uzzaircode@gmail.com>
2019-01-28 19:07:14 +0800

    m

--- resources/views/backend/partials/footer.blade.php
+++ resources/views/backend/partials/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="site-footer">
     <div class="row">
         <div class="col-md-6">
-            <p class="text-center text-md-left">Copyright © {{Carbon\Carbon::now()->format('Y')}} . Datakraf Solutions Sdn. Bhd. All rights reserved</p>
+            <p class="text-center text-md-left">Copyright © {{Carbon\Carbon::now()->format('Y')}} . Datakraf Solution Sdn. Bhd. All rights reserved</p>
         </div>        
     </div>
 </footer>
