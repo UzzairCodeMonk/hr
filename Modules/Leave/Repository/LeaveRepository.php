00b119ebbb3d7a067ae6bd11e0ad751fb77d5277
Uzzair Baharudin <uzzaircode@gmail.com>
2019-03-01 11:14:24 +0800

    place star rating in resume view

---
+++ Modules/Leave/Repository/LeaveRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Leave\Repository;
+
+use Datakraf\User;
+use Uzzaircode\DateHelper\Traits\DateHelper;
+
+class LeaveRepository{
+
+    public function getUserStatus(int $id): string
+    {
+        return User::find($id)->personalDetail->status;
+    }
+
+    public function checkUserStatus(string $string)
+    {
+        if()
+    }
+}
