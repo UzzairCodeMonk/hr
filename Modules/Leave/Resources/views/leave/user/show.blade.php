880a40f228deb0371a421dcee0b113f40e06e4ed
Uzzair Baharudin <uzzaircode@gmail.com>
2019-03-07 09:40:11 +0800

    capitalize period

--- Modules/Leave/Resources/views/leave/user/show.blade.php
+++ Modules/Leave/Resources/views/leave/user/show.blade.php
@@ -72,7 +72,7 @@ Leave Application Form
                         <div class="col">
                             <div class="form-group">
                                 <label for="input-normal">{{ucwords(__('leave::leave.days-taken'))}}</label>
-                                <p>{{$leave->days_taken ?? 'N/A'}} ({{$leave->period ?? ''}})</p>
+                                <p>{{$leave->days_taken ?? 'N/A'}} ({{ucwords($leave->period ?? '')}})</p>
                             </div>
                         </div>
                         <div class="col">
