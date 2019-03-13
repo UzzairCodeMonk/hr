c83e6db324e91987f09daa180f59b01dd8d3ed5c
Uzzair Baharudin <uzzaircode@gmail.com>
2019-03-13 10:37:39 +0800

    downgrade consoletv/charts to version 5

--- Modules/Leave/Resources/views/leave/admin/dashboard.blade.php
+++ Modules/Leave/Resources/views/leave/admin/dashboard.blade.php
@@ -28,15 +28,41 @@ Dashboard
     }
 
 </style>
+{!! Charts::assets() !!}
 @endsection
 @section('content')
 
 <div class="col-md-8 mx-auto">
-    <div class="row h-100 justify-content-center align-items-center">        
-        <div class="col mx-auto">
-            <img src="{{asset('images/dashboard-image.png')}}" alt="" class="" height="500px">
+    <div class="row h-100 justify-content-center align-items-center">
+        <div class="col">
+            <div class="">
+                <h1 class="display-5">Leave Modules</h1>
+                <!-- <p class="display-6">Here's what's happening in Datakraf Solution Sdn. Bhd.</p> -->
+            </div>
         </div>
-    </div>    
+        <div class="col mx-auto">
+            <img src="{{asset('images/dashboard/requests.svg')}}" alt="" class="" height="400px">
+        </div>
+    </div>
+    <div class="row">
+            <div class="col">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Who's not in the office?</h3>
+                        <div class="card-options">
+                            <select name="" id="daysSelector" class="form-control">
+                                <option value="7">In 7 Days</option>
+                                <option value="30">In 30 Days</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                            {!! $chart->render() !!}
+                    </div>
+                </div>
+    
+            </div>
+        </div>
     <div class="row">
         <div class="col">
             <div class="card">
@@ -82,5 +108,8 @@ Dashboard
         </div>
     </div>
 </div>
+
+@endsection
+@section('page-js')
 
 @endsection
---
+++ app/Charts/LeaveApplicationsByMonth.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Datakraf\Charts;
+
+use ConsoleTVs\Charts\Classes\Chartjs\Chart;
+
+class LeaveApplicationsByMonth extends Chart
+{
+    /**
+     * Initializes the chart.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
